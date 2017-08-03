032a1d443f905d4ab0a88c63e5de210693218914
Jenya Y <jenyay@gmail.com>
2017-08-03 12:09:37 +0300

    Store mapping face <=> user after tagging

--- Tagging/FinalizeTag.cs
+++ Tagging/FinalizeTag.cs
@@ -25,22 +25,44 @@ namespace Tagging
             log.Info($"Queue trigger '{nameof(CleanupTag)}' with message: {message}");
             var slackInput = message.FromJson<TaggingMessage>();
 
-            using (var request = new HttpRequestMessage(HttpMethod.Post, slackInput.ResponseUrl))
+            await Task.WhenAll(
+                storeFaceUserMapping(slackInput.FaceId, slackInput.UserId),
+                sendSlackMessage(slackInput.FaceId, slackInput.UserId, slackInput.ResponseUrl, log));
+        }
+
+        private static async Task storeFaceUserMapping(string faceId, string userId)
+        {
+            if (String.IsNullOrEmpty(faceId)) throw new ArgumentNullException(nameof(faceId));
+            if (String.IsNullOrEmpty(userId)) throw new ArgumentNullException(nameof(userId));
+
+            using (var connection = new SqlConnection(Settings.SQL_CONN_STRING))
             {
-                var payload = getMessage(
-                    slackInput.FaceId,
-                    (await getName(slackInput.UserId))?.FirstName)
+                await connection.OpenAsync();
+                await connection.ExecuteAsync(
+                    "UPDATE [dbo].[Faces] SET UserId = @UserId WHERE Id = @FaceId",
+                    new { FaceId = faceId, UserId = userId });
+            }
+        }
+
+        private static async Task sendSlackMessage(string faceId, string userId, string responseUrl, TraceWriter log)
+        {
+            if (String.IsNullOrEmpty(faceId)) throw new ArgumentNullException(nameof(faceId));
+            if (String.IsNullOrEmpty(userId)) throw new ArgumentNullException(nameof(userId));
+            if (String.IsNullOrEmpty(responseUrl)) throw new ArgumentNullException(nameof(responseUrl));
+
+            using (var request = new HttpRequestMessage(HttpMethod.Post, responseUrl))
+            {
+                var payload = getMessage(faceId, (await getMapping(userId))?.FirstName)
                     .ToJson(camelCasingMembers: true);
+
                 request.Content = new StringContent(payload, Encoding.UTF8, "application/json");
                 var response = await _client.SendAsync(request);
                 log.Info($"Sent to Slack {payload} and received from service {response.StatusCode}: {await response.Content.ReadAsStringAsync()}");
             }
         }
 
-        private static async Task<UserMap> getName(string userId)
+        private static async Task<UserMap> getMapping(string userId)
         {
-            if (String.IsNullOrEmpty(userId)) throw new ArgumentNullException(nameof(userId));
-
             using (var connection = new SqlConnection(Settings.SQL_CONN_STRING))
             {
                 await connection.OpenAsync();
