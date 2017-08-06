16a14541bec8328c54fa4bc05db50a49221ef6c5
Jenya Y <jenyay@gmail.com>
2017-08-06 16:44:49 +0300

    Clear wrong identify from DB

---
+++ Recognition/ClearIdentified.cs
@@ -0,0 +1,31 @@
+using Common;
+using Dapper;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.ServiceBus.Messaging;
+using Recognition.Models;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Recognition
+{
+    public static class ClearIdentified
+    {
+        [FunctionName(nameof(ClearIdentified))]
+        public static async Task Run(
+            [ServiceBusTrigger("atmosphere-face-undo-identify", AccessRights.Listen, Connection = Settings.SB_CONN_NAME)]string message,
+            TraceWriter log)
+        {
+            log.Info($"Queue trigger '{nameof(ClearIdentified)}' with message: {message}");
+            var undoMessage = message.FromJson<UndoIdentifyMessage>();
+
+            using (var connection = new SqlConnection(Settings.SQL_CONN_STRING))
+            {
+                await connection.OpenAsync();
+                await connection.ExecuteScalarAsync<int>(
+                    "UPDATE [dbo].[Faces] SET UserId = NULL WHERE Id = @FaceId",
+                    new { FaceId = undoMessage.FaceId });
+            }
+        }
+    }
+}
